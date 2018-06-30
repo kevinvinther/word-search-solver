140d12c249a3fb4527421f32bf9765e479b1c82b
Kevin <kevinjoshua.kjv@gmail.com>
2018-06-30 16:28:54 +0200

    Now uses googles c++ style guide (from clang-format), also started work on algorithm, needs to actually *find the word*

--- main.cpp
+++ main.cpp
@@ -1,85 +1,110 @@
+#include <algorithm>
+#include <fstream>  //For input output
 #include <iostream>
-#include <fstream> //For input output
 #include <string>
 #include <vector>
-#include <algorithm>
 
 //
-// ──────────────────────────────────────────────────────────────────────────── I ──────────
-//   :::::: W O R D   S E A R C H   S O L V E R : :  :   :    :     :        :          :
+// ─────────────────────────────────────────────────────────────────────────────────────────
+//   :::::: W O R D   S E A R C H   S O L V E R : :  :   :    :     :        : :
 // ──────────────────────────────────────────────────────────────────────────────────────
 //
 
+void SearchForWordAtGivenLocation(std::vector<std::string> table,
+                                  std::string word) {
+    for (size_t word_progress = 0; word_progress < word.size();
+         word_progress++) {
+        // Search for word in one direction
+    }
+}
+
 int main(int argc, char const *argv[]) {
-	//Opens the file, creates an object called "puzzle" for the file.
-	std::ifstream puzzle;
-	puzzle.open("puzzle.txt");
-	std::ifstream words;
-	words.open("words.txt");
+    // Opens the file, creates an object called "puzzle" for the file.
+    std::ifstream puzzle;
+    puzzle.open("puzzle.txt");
+    std::ifstream words;
+    words.open("words.txt");
 
-	//Check for error, to return either false or true
-	if (puzzle.fail()) {
-		std::cerr << "There was an error opening the file, did you use puzzle.txt?" << std::endl;
-		return 1;
-	}
-	if (words.fail()) {
-		std::cerr << "There was an error opening the file, did you use words.txt?" << std::endl;
-		return 1;
-	}
+    // Check for error, to return either false or true
+    if (puzzle.fail()) {
+        std::cerr
+            << "There was an error opening the file, did you use puzzle.txt?"
+            << std::endl;
+        return 1;
+    }
+    if (words.fail()) {
+        std::cerr
+            << "There was an error opening the file, did you use words.txt?"
+            << std::endl;
+        return 1;
+    }
 
-	//Input the table
-	std::vector<std::string> letter_table;
-	while (!puzzle.eof()) {
-		std::string temp;
-		puzzle >> temp;
-		std::transform(temp.begin(), temp.end(), temp.begin(), ::tolower);
-		letter_table.push_back(temp);
-	}
+    // Input the table
+    std::vector<std::string> letter_table;
+    while (!puzzle.eof()) {
+        std::string temp;
+        puzzle >> temp;
+        std::transform(temp.begin(), temp.end(), temp.begin(), ::tolower);
+        letter_table.push_back(temp);
+    }
 
-	//Calculate the height and width
-	const int table_height = letter_table.size(),
-		table_width = letter_table[0].size();
+    // Calculate the height and width
+    const int table_height = letter_table.size(),
+              table_width = letter_table[0].size();
 
+    // Input the words
+    std::vector<std::string> word_list;
+    int word_count;
+    while (!words.eof()) {
+        std::string temp;
+        words >> temp;
+        std::transform(temp.begin(), temp.end(), temp.begin(), ::tolower);
+        word_list.push_back(temp);
+    }
+    word_count = word_list.size();
 
-	//Input the words
-	std::vector<std::string> word_list;
-	int word_count;
-	while(!words.eof()) {
-		std::string temp;
-		words >> temp; 
-		std::transform(temp.begin(), temp.end(), temp.begin(), ::tolower);
-		word_list.push_back(temp); 
-	}
-	word_count = word_list.size();
+    // Print the puzzle
+    for (int i = 0; i < letter_table.size(); i++) {
+        std::cout << letter_table.at(i) << std::endl;
+    }
 
-	//Main loop
-	for (size_t current_word_index = 0; current_word_index < word_count; current_word_index++) {
-		std::cout << "Finding word: " << word_list[current_word_index] << std::endl;
-		for (int direction_tested = 0; direction_tested < 8; direction_tested++) {
-			if (valid_directions[direction_tested] == 0)
-				break;
-			else {
-				// CODE WHOMST SEARCHES FOR THE WORD IN THAT DIRECTION
-				// and then
-				// CODE WHOMST DISPLAYS THE FOUND WORD AND ITS LOCATION
-			}
-		}
-	}
+    // Main loop
+    for (size_t word_progress = 0; word_progress < word_count;
+         word_progress++) {
+        std::string word = word_list[word_progress];
+        bool word_found = false;
+        std::cout << "Searching for word " << word << std::endl;
 
-	//Tests
-	std::cout << "Table width: " << table_width << std::endl
-			  << "Table height: " << table_height << std::endl
-			  << "Chosen directions: " << std::endl;
-	/*
-	for (int i = 0; i < 8; i++) {
-		std::cout << valid_directions[i] << ' ';
-	}
-	*/
-	std::cout << std::endl 
-			  << "Words: " << std::endl; 
-	for(int i = 0; i < word_list.size(); i++) {
-		std::cout << word_list[i] << std::endl; 
-	}
+        if (word_found != true) {
+            for (size_t height_progress = 0; height_progress < table_height;
+                 height_progress++) {
+                for (size_t width_progress = 0; width_progress < table_width;
+                     width_progress++) {
+                    if (letter_table.at(height_progress).at(width_progress) ==
+                        word_list[word_progress].at(0)) {
+                        std::cout << "Found!" << std::endl;
+                        // Search on for word
+                        for (size_t directions = 0; directions < 8;
+                             directions++) {
+                            // LookForWord
+                        }
 
-	return 0;
+                        continue;
+                    }
+                }
+            }
+        }
+    }
+
+    // Tests
+    std::cout << "Table width: " << table_width << std::endl
+              << "Table height: " << table_height << std::endl
+              << "Chosen directions: " << std::endl;
+
+    std::cout << std::endl << "Words: " << std::endl;
+    for (int i = 0; i < word_list.size(); i++) {
+        std::cout << word_list[i] << std::endl;
+    }
+
+    return 0;
 }
